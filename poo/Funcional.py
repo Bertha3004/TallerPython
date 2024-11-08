6e125a3819f7f02b6149d2c807534799d8da9297
Bertha Solis Cortes <berthasolis902@gmail.com>
2024-11-08 10:14:08 -0600

    se agrego funcion lambda2

--- poo/Funcional.py
+++ poo/Funcional.py
@@ -1,3 +1,14 @@
+import math
+
 if __name__ == '__main__':
+
     Suma =lambda x,y:x+y
     print(f"la suma es: {Suma(2,6)}")
+
+    Resta = lambda x, y: x - y
+    print(f"la resta es: {Resta(3,4)}")
+
+    Potencia = lambda x: x**2
+    print(f"la potencia es: {Potencia(9)}")
+
+    x1 = lambda a, b, c: (-b + math.sqrt(b**2*4*a*c)/2*a)
